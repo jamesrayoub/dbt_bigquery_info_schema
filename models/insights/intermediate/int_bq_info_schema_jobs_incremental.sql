08f638d8f05e2051288932498a4776a192589bb6
jamesrayoub <jamesrayoub@gmail.com>
2023-04-07 11:02:05 -0400

    switch to dynamic partitions

--- models/insights/intermediate/int_bq_info_schema_jobs_incremental.sql
+++ models/insights/intermediate/int_bq_info_schema_jobs_incremental.sql
@@ -1,21 +1,16 @@
-{% set partitions_to_replace = [
-  'timestamp(current_date)',
-  'timestamp(date_sub(current_date, interval 1 day))'
-] %}
-
 {{
   config(
     materialized = 'incremental',
     incremental_strategy = 'insert_overwrite',
     partition_by = {'field': 'created_at', 'data_type': 'timestamp'},
-    partitions = partitions_to_replace,
     cluster_by = ['user_email','job_type']
   )
 }}
 
-select * from {{ ref('stg_bq_info_schema__jobs') }}
+select * 
+from {{ ref('stg_bq_info_schema__jobs') }}
 
 {% if is_incremental() %}
--- recalculate yesterday + today
-where timestamp_trunc(created_at, day) in ({{ partitions_to_replace | join(',') }})
+-- recalculate latest day's data + previous
+where date(created_at) >= date_sub(date(_dbt_max_partition), interval 1 day)
 {% endif %}
--- models/insights/intermediate/int_bq_info_schema_jobs_timeline_incremental.sql
+++ models/insights/intermediate/int_bq_info_schema_jobs_timeline_incremental.sql
@@ -1,21 +1,16 @@
-{% set partitions_to_replace = [
-  'timestamp(current_date)',
-  'timestamp(date_sub(current_date, interval 1 day))'
-] %}
-
 {{
   config(
     materialized = 'incremental',
     incremental_strategy = 'insert_overwrite',
     partition_by = {'field': 'job_created_at', 'data_type': 'timestamp'},
-    partitions = partitions_to_replace,
     cluster_by = ['user_email','job_type']
   )
 }}
 
-select * from {{ ref('stg_bq_info_schema__jobs_timeline') }}
+select * 
+from {{ ref('stg_bq_info_schema__jobs_timeline') }}
 
 {% if is_incremental() %}
--- recalculate yesterday + today
-where timestamp_trunc(job_created_at, day) in ({{ partitions_to_replace | join(',') }})
+-- recalculate latest day's data + previous
+where date(job_created_at) >= date_sub(date(_dbt_max_partition), interval 1 day)
 {% endif %}
