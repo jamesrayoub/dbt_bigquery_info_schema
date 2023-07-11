dc1f0093f4b3d81932c77837ccb55298256dabae
jamesrayoub <jamesrayoub@gmail.com>
2023-07-11 11:48:28 -0400

    move int model to cte

--- models/insights/table_stats.sql
+++ models/insights/table_stats.sql
@@ -1,3 +1,32 @@
+with table_references as (
+
+    select
+        ref_tables.project_id,
+        ref_tables.dataset_id,
+        ref_tables.table_id,
+        countif(date(created_at) >= date_sub(current_date, interval 30 day)) as last_30_day_query_references,
+        countif(date(created_at) >= date_sub(current_date, interval 90 day)) as last_90_day_query_references,
+        countif(date(created_at) >= date_sub(current_date, interval 180 day)) as last_180_day_query_references
+
+    from {{ ref('int_bq_info_schema_jobs_incremental') }},
+
+    unnest(referenced_tables) as ref_tables
+
+    where date(created_at) >= date_sub(current_date, interval 180 day)
+        and job_type = 'QUERY'
+        {%- if var('target_query_users',false) %}
+        and user_email in (
+        {%- for user in var('target_query_users') %}
+            '{{ user }}' {%- if not loop.last -%} , {%- endif -%}
+        {%- endfor %}
+        )
+        {%- endif %}
+
+    {{ dbt_utils.group_by(n=3) }}
+
+)
+
+
 select 
     tables.project_id,
     tables.dataset_id,
@@ -28,7 +57,7 @@ inner join {{ ref('stg_bq_info_schema__table_storage') }} as table_storage
     and tables.dataset_id = table_storage.dataset_id
     and tables.table_id = table_storage.table_id
 
-left join {{ ref('int_bq_info_schema_table_query_references') }} as table_references
+left join table_references
     on tables.project_id = table_references.project_id
     and tables.dataset_id = table_references.dataset_id
     and tables.table_id = table_references.table_id
